43cc27d6cb998ac5515ccc1ebfcac1dfc0fc9328
german664 <german.mora.664@gmail.com>
2020-08-03 19:10:28 +0000

    2nd commit

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,27 +1,20 @@
-let colorname = prompt('What color do you want?');
-let isAvailable = getColor(colorname);
-
 function getColor(selection)
 {
 	switch(selection){
         // Add more options here
-        case 'red':
-            return true;
-        break;
-        case 'green':
-            return true;
-        break;
-        case 'blue':
-            return true;
-        break;
+        case "red": return true;
+        case "green": return true;
+        case "blue": return true;
 	    default:
-	    	return false;  //returns false because the user picked an unavailable color            
+	    	return false;  //returns false because the user picked an unavailable color
+	    break;               
 	}
 }
 
+var colorname = prompt('What color do you want?').toLowerCase();
+var isAvailable = getColor(colorname);
 
-if(isAvailable === true)
+if(isAvailable)
 	console.log('Good news! That color is available');
 else 
 	console.log('We are sorry, that color is not available');
-
--- exercises/22-Russian-Roulette/app.js
+++ exercises/22-Russian-Roulette/app.js
@@ -1,24 +1,15 @@
-var bulletPosition =  4;
+var bulletPosition = 4;
 
 const spinChamber = () => {
 	var chamberPosition = Math.floor((Math.random() * 6) + 1);
 	return chamberPosition;
 };
 
-let chamber =  spinChamber();
-
 // Remove the // below and complete the commented lines
-const fireGun = (bullet, spinnerPosition) => {
-
-if(bullet === spinnerPosition ) {
-    return 'You\'re dead!' 
-} else {
-return "Keep playing!"
-}
+const fireGun = (spinnerPosition) => {
+    if (bulletPosition === spinnerPosition) {
+        return "You're dead!";
+    } else { return "Keep playing!"}
 };
 
-console.log(fireGun(bulletPosition, chamber))
-console.log(chamber)
-console.log(bulletPosition)
-
-//console.log(fireGun(...));
+console.log(fireGun(spinChamber()))
